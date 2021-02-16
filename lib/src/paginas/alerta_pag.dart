d1a4e4ae365271c94830219ecd412900dc2022b4
Rene Aguiña <renecrapaud@gmail.com>
2021-02-15 22:41:39 -0600

    Agregar widget de alerta, con imagen y botones

--- lib/src/paginas/alerta_pag.dart
+++ lib/src/paginas/alerta_pag.dart
@@ -7,12 +7,53 @@ class AlertPage extends StatelessWidget{
       appBar: AppBar(
         title: Text('Pagina de alerta'),
       ),
+      body: Center(
+        child: RaisedButton(
+          child: Text('Ejecutar acción'),
+          color: Colors.blue,
+          textColor: Colors.white,
+          shape: StadiumBorder(),
+          onPressed: (){
+            _mostrarAlerta(context);
+          },
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.call_end),
         onPressed: (){
           Navigator.pop(context);
         },
       ),
+    );
+  }
+  
+  void _mostrarAlerta(BuildContext context){
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (context) {
+        return AlertDialog(
+          shape: RoundedRectangleBorder( borderRadius: BorderRadius.circular(20.0)),
+          title: Text('Titulo'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Text('contenido'),
+              FlutterLogo(size: 100.0,),
+            ],
+          ),
+          actions: <Widget>[
+            FlatButton(
+              child: Text('Cancelar'),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+            FlatButton(
+              child: Text('Ok'),
+              onPressed: () => Navigator.of(context).pop(),
+            )
+          ],
+        );
+      }
     );
   }
 }
