00b6a639f9ff7cca79b75ef370b23c130fe91248
Rene Aguiña <renecrapaud@gmail.com>
2021-02-17 18:35:21 -0600

    Agregar página de contenedor animado

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:componentes_app/src/paginas/contenedor_animado.dart';
 import 'package:componentes_app/src/paginas/alerta_pag.dart';
 import 'package:componentes_app/src/paginas/avatar_pag.dart';
 import 'package:componentes_app/src/paginas/home_page.dart';
@@ -9,6 +10,7 @@ Map<String, WidgetBuilder> getApplicationRoutes(){
     '/' : (BuildContext context) => HomePage(),
     'alert' : (BuildContext context) => AlertPage(),
     'avatar' : (BuildContext context) => AvatarPage(),
-    'card' : (BuildContext context) => CardPage()
+    'card' : (BuildContext context) => CardPage(),
+    'animatedContainer' : (BuildContext context) => AnimatedContainerPage()
   };
 }
