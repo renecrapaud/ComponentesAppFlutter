2e9bbcde09bacdab6c57b69734bed2f2c8c16d49
Rene Aguiña <renecrapaud@gmail.com>
2021-02-18 19:02:08 -0600

    Pagina entradas. Agregar fecha

--- lib/src/paginas/input_pag.dart
+++ lib/src/paginas/input_pag.dart
@@ -6,7 +6,10 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
-  String _nombre;
+  String _nombre = '';
+  String _email = '';
+  String _fecha = '';
+  TextEditingController _controlEntradaFecha = new TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,6 +20,12 @@ class _InputPageState extends State<InputPage> {
         padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
         children: <Widget>[
           _agregaEntrada(),
+          Divider(),
+          _agregaEntradaCorreo(),
+          Divider(),
+          _agrEntContrasena(),
+          Divider(),
+          _agregaEntradaFecha(context),
           Divider(),
           _copiarNombre(),
         ],
@@ -50,7 +59,79 @@ class _InputPageState extends State<InputPage> {
   Widget _copiarNombre() {
     return ListTile(
       title: Text('Nombre es: $_nombre'),
+      subtitle: Text('Email: $_email'),
     );
+  }
+
+  Widget _agregaEntradaCorreo() {
+    return TextField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20.0),
+        ),
+        hintText: 'Dirección de correo',
+        labelText: 'Correo electrónico',
+        suffixIcon: Icon(Icons.alternate_email),
+        icon: Icon(Icons.email),
+      ),
+      onChanged: (valor){
+        setState(() {
+          _email = valor;
+        });
+      },
+    );
+  }
+
+  Widget _agrEntContrasena() {
+    return TextField(
+      obscureText: true,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20.0),
+        ),
+        hintText: 'Contraseña',
+        labelText: 'Contraseña',
+        suffixIcon: Icon(Icons.lock_open),
+        icon: Icon(Icons.lock),
+      ),
+    );
+  }
+
+  Widget _agregaEntradaFecha(BuildContext context) {
+    return TextField(
+      controller: _controlEntradaFecha,
+      enableInteractiveSelection: false,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20.0),
+        ),
+        hintText: 'Fecha de nacimiento',
+        labelText: 'Fecha de nacimiento',
+        suffixIcon: Icon(Icons.perm_contact_calendar),
+        icon: Icon(Icons.calendar_today),
+      ),
+      onTap: (){
+        FocusScope.of(context).requestFocus(new FocusNode());
+        _seleccionaFecha(context);
+      },
+    );
+  }
+
+  void _seleccionaFecha(BuildContext context) async {
+    DateTime seleccionada = await showDatePicker(
+        context: context,
+        initialDate: new DateTime.now(),
+        firstDate: new DateTime(2019),
+        lastDate: new DateTime(2025)
+    );
+
+    if(seleccionada != null){
+      setState(() {
+        _fecha = seleccionada.toString();
+        _controlEntradaFecha.text = _fecha;
+      });
+    }
   }
 
 }
